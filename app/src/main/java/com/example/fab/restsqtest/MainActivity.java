c6f097c367df49e9dfbc2e9be07d2272354f98bc
fab <fabvest7@gmail.com>
2016-10-01 20:48:20 +0300

    ggg

--- app/src/main/java/com/example/fab/restsqtest/MainActivity.java
+++ app/src/main/java/com/example/fab/restsqtest/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements Callback<User> {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //loggingjj
+        //loggingjjkP
 
         retrofit = new Retrofit.Builder()
                 .baseUrl("http://shopquest-rudolf.rhcloud.com/api/")
